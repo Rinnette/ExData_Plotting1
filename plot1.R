cedc6d8e1aafd81b8a979b7158d44c5adf803cbb
Rinnette Ramdhanie <rinnette@gmail.com>
2019-07-20 11:57:21 -0400

    Add sep as semi-colon and filter required dates

--- plot1.R
+++ plot1.R
@@ -1,3 +1,4 @@
 library(dplyr)
-hpc <- tbl_df(read.table("household_power_consumption.txt", header = TRUE, na.strings = "?", nrows = 100))
+hpc <- tbl_df(read.table("household_power_consumption.txt", header = TRUE, sep = ";", na.strings = "?", nrows = 2075259))
+hpcdata <- filter(hpc, Date %in% c("1/2/2007", "2/2/2007"))
 as.Date(hpc$Date, format = "%m/%d/%Y")
