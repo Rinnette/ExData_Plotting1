1f1884be2d001c3aa50ab282296cea5d5c373221
Rinnette Ramdhanie <rinnette@gmail.com>
2019-07-20 12:43:12 -0400

    Create histogram

--- plot1.R
+++ plot1.R
@@ -1,4 +1,20 @@
-library(dplyr)
-hpc <- tbl_df(read.table("household_power_consumption.txt", header = TRUE, sep = ";", na.strings = "?", nrows = 2075259))
-hpcdata <- filter(hpc, Date %in% c("1/2/2007", "2/2/2007"))
-as.Date(hpc$Date, format = "%m/%d/%Y")
+# Read data 
+        library(dplyr)
+        hpc <- tbl_df(read.table("household_power_consumption.txt", header = TRUE, sep = ";", 
+                                 na.strings = "?", nrows = 2075259))
+        
+# Filter data with dates between 1/2/2007 and 2/2/2007        
+        hpcdata <- filter(hpc, Date %in% c("1/2/2007", "2/2/2007"))
+        rm(hpc) # remove the original dataset
+        
+# Change the Date column to Date class        
+        hpcdata$Date <- as.Date(hpcdata$Date, format = "%m/%d/%Y")
+
+# Create histogram and save to PNG file
+        png("ExData_Plotting1/plot1.png", width = 480, height = 480)
+        hist(hpcdata$Global_active_power, main = "Global Active Power", 
+             xlab = "Global Active Power (kilowatts)", col = "red")
+        dev.off()
+
+
+        
